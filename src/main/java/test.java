f4a5c19309c923d2fc45a6b3fe702479e9a06147
Maaz <maazahmedks@gmail.com>
2019-05-07 11:07:41 -0700

    Last

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,8 @@
 public class test {
 
 public void test(){
-    System.out.println("tesstingz");
-    System.out.println("2 tesstingz");
-    System.out.println("3 tesstingz");
+    System.out.println("tessting 1");
+
 }
 
 }
